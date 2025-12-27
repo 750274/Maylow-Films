e0e39c6a051346f7ea8856944ca1430e37428717
Joao Vitor Teixeira <Jvtgoulart@gmail.com>
2025-12-27 19:51:54 -0300

    Primeiro commit - site Maylow Films

---
+++ script.js
@@ -0,0 +1,23 @@
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                if (target) {
+                    const headerOffset = 80;
+                    const elementPosition = target.getBoundingClientRect().top;
+                    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+                    window.scrollTo({
+                        top: offsetPosition,
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
+
+        const productButtons = document.querySelectorAll('.product-button');
+        productButtons.forEach(button => {
+            button.addEventListener('click', () => {
+                window.location.href = '#contato';
+            });
+        });
