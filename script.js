6436bf897dcad82fde8de80444761184312197a7
Joao Vitor Teixeira <Jvtgoulart@gmail.com>
2025-12-27 20:32:45 -0300

    Alteração mobile

--- script.js
+++ script.js
@@ -16,8 +16,13 @@
         });
 
         const productButtons = document.querySelectorAll('.product-button');
-        productButtons.forEach(button => {
-            button.addEventListener('click', () => {
-                window.location.href = '#contato';
-            });
-        });
+
+productButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        window.open(
+            'https://wa.me/5551999999999?text=Olá!%20Gostaria%20de%20solicitar%20um%20orçamento%20para%20insulfilm%20automotivo.',
+            '_blank'
+        );
+    });
+});
+
